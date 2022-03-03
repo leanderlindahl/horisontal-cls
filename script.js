e7e30aac458f09cc5b9d26b3997dce367559fa08
Leander Lindahl <leander.lindahl@ingka.ikea.com>
2022-03-03 09:56:11 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const thePostalPicker = document.querySelector(".pick-postal");
+const theStorePicker = document.querySelector(".pick-store");
+
+const postalEventHandler = (event) => {
+  thePostalPicker.querySelector('.textwrapper .text').innerHTML = 'Lorem ipsum dolor sit amet consectetuer';
+};
+const storeEventHandler = (event) => {
+  theStorePicker.querySelector('.textwrapper .text').innerHTML = 'The quick brown fox jumped';
+};
+
+setTimeout(() => {postalEventHandler()}, 1000);
+setTimeout(() => {storeEventHandler()}, 1000);
